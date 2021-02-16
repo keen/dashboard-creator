f1afecca98b5ecd08040ac474ae1b6e62bc5e716
dariuszlacheta <dariusz.lacheta@keen.io>
2021-02-16 14:52:52 +0100

    fix: 🐛 Fix for unexpected scrolling while tag is clicked

--- src/components/FilterDashboards/FilterDashboards.styles.ts
+++ src/components/FilterDashboards/FilterDashboards.styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { transparentize } from 'polished';
-import { layout, LayoutProps, system } from 'styled-system';
 import { UI_LAYERS } from '@keen.io/ui-core';
 import { colors } from '@keen.io/colors';
 
@@ -17,17 +16,8 @@ export const Filter = styled.div`
   padding: 10px 14px;
 `;
 
-export const DropdownContainer = styled.div<
-  { customTransform: string } & LayoutProps
->`
+export const DropdownContainer = styled.div`
   z-index: ${UI_LAYERS.dropdown};
-  position: absolute;
-  ${layout}
-  ${system({
-    customTransform: {
-      property: 'transform',
-    },
-  })}
 `;
 
 export const DropdownContent = styled.div`
