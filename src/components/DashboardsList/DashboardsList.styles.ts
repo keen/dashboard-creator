9b56406fe607ee93805ffc7124ea8557d0c3753a
Artur Pryka <artur.pryka@keen.io>
2021-04-22 16:23:22 +0200

    fix: 🐛 limit DashboardTile width

--- src/components/DashboardsList/DashboardsList.styles.ts
+++ src/components/DashboardsList/DashboardsList.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const DashboardsGrid = styled.div`
   display: grid;
   grid-gap: 20px;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(300px, 380px));
 `;
 
 export const DashboardItem = styled.div`
