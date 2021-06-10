baed17e8668fc2e54cfa0f8db965a840da5e98f5
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-06-10 09:06:15 +0200

    fix: 🐛 Mock implementation changed

--- src/components/ThemeEditor/components/ColorSelector/ColorSelector.test.tsx
+++ src/components/ThemeEditor/components/ColorSelector/ColorSelector.test.tsx
@@ -20,20 +20,13 @@ const render = (overProps: any = {}) => {
 
 beforeEach(() => {
   HTMLCanvasElement.prototype.getContext = jest.fn();
-  spyOn(Element.prototype, 'getBoundingClientRect').and.callFake(
-    jasmine
-      .createSpy('getBoundingClientRect')
-      .and.returnValue({
-        x: 1,
-        y: 1,
-        top: 1,
-        left: 1,
-        right: 1,
-        bottom: 1,
-        height: 100,
-        width: 100,
-      })
-  );
+  Element.prototype.getBoundingClientRect = jest
+    .fn()
+    .mockImplementation(() => ({
+      x: 1,
+      y: 1,
+      height: 100,
+    }));
 });
 
 test('should render color selector with defined color', () => {
