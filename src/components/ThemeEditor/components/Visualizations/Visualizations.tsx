70c34f55bf734bf9e341969a99f2d7ce51111a26
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-07-06 19:10:20 +0200

    feat: 🎸 table settings

--- src/components/ThemeEditor/components/Visualizations/Visualizations.tsx
+++ src/components/ThemeEditor/components/Visualizations/Visualizations.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { Theme } from '@keen.io/charts';
 
-import { CircularChart, Funnel, Metric } from './components';
+import { CircularChart, Funnel, Metric, Table } from './components';
 import SettingsDivider from '../SettingsDivider';
 
 import { ThemeSettings, themeSelectors } from '../../../../modules/theme';
@@ -20,7 +20,7 @@ type Props = {
 
 const Visualizations: FC<Props> = ({ currentSettings, onUpdateSettings }) => {
   const {
-    theme: { donut, pie, funnel, metric },
+    theme: { donut, pie, funnel, metric, table },
     settings: dashboardSettings,
   } = currentSettings;
 
@@ -48,6 +48,14 @@ const Visualizations: FC<Props> = ({ currentSettings, onUpdateSettings }) => {
         settings={metric}
         onChange={(metricSettings) =>
           onUpdateSettings({ metric: metricSettings }, dashboardSettings)
+        }
+        colorSuggestions={defaultColors}
+      />
+      <SettingsDivider />
+      <Table
+        settings={table}
+        onChange={(tableSettings) =>
+          onUpdateSettings({ table: tableSettings }, dashboardSettings)
         }
         colorSuggestions={defaultColors}
       />
--- src/components/ThemeEditor/components/Visualizations/components/index.ts
+++ src/components/ThemeEditor/components/Visualizations/components/index.ts
@@ -1,5 +1,6 @@
 import CircularChart from './CircularChart';
 import Funnel from './Funnel';
 import Metric from './Metric';
+import Table from './Table';
 
-export { CircularChart, Metric, Funnel };
+export { CircularChart, Metric, Funnel, Table };
