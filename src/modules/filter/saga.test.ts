7ceeb42de664111f7892812b7ed6c98710e85f4c
Michał Czapkowicz <michal.czapkowicz@keen.io>
2021-07-12 15:04:56 +0200

    fix: 🐛 Detached widgets error fix added

--- src/modules/filter/saga.test.ts
+++ src/modules/filter/saga.test.ts
@@ -568,4 +568,75 @@ describe('getDetachedFilterWidgetConnections()', () => {
       ]);
     });
   });
+
+  describe('Scenario 3: Dont add widgets with error state to detached filter widget connections', () => {
+    const dashboardId = '@dashboard/01';
+    const filterWidgetId = '@filter/01';
+
+    const state = {
+      widgets: {
+        items: {
+          '@widget/01': {
+            error: {},
+            widget: {
+              id: '@widget/01',
+              type: 'visualization',
+              filterIds: [filterWidgetId],
+              position: { y: 10 },
+              settings: {
+                widgetSettings: {},
+              },
+            },
+            data: null,
+          },
+          '@widget/02': {
+            widget: {
+              id: '@widget/02',
+              type: 'visualization',
+              filterIds: [filterWidgetId],
+              position: { y: 5 },
+              settings: {
+                widgetSettings: {},
+              },
+            },
+            data: null,
+          },
+        },
+      },
+      dashboards: {
+        items: {
+          [dashboardId]: {
+            settings: { widgets: ['@widget/01', '@widget/02'] },
+          },
+        },
+      },
+    };
+
+    function* wrapper() {
+      const result = yield* getDetachedFilterWidgetConnections(
+        dashboardId,
+        filterWidgetId,
+        'purchases'
+      );
+      return result;
+    }
+
+    const test = sagaHelper(wrapper());
+
+    test('get application state', (result) => {
+      expect(result).toEqual(select());
+      return state;
+    });
+
+    test('returns filter widget connections', (result) => {
+      expect(result).toEqual([
+        {
+          isConnected: true,
+          positionIndex: 2,
+          title: null,
+          widgetId: '@widget/02',
+        },
+      ]);
+    });
+  });
 });
