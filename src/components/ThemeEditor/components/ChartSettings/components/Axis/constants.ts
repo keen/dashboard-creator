9ebee132a7e84bf211963864822ea28963fb4077
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2021-08-04 09:29:48 +0200

    feat: 🎸 axis titles
    support for providing axis title from user interface

--- src/components/ThemeEditor/components/ChartSettings/components/Axis/constants.ts
+++ src/components/ThemeEditor/components/ChartSettings/components/Axis/constants.ts
@@ -7,4 +7,13 @@ export const AXIS_LABELS_SETTINGS = {
   alignment: false,
 };
 
+export const AXIS_TITLES_SETTINGS = {
+  color: true,
+  fontSize: true,
+  bold: true,
+  italic: true,
+  underline: false,
+  alignment: false,
+};
+
 export const AVAILABLE_FONT_SIZES = [9, 10, 11, 12, 13, 14, 15, 16];
